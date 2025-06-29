ba5318db64ea13bcc8cae9963b5247230588cf1d
Muhendalf <lesnik1234567895@mail.ru>
2025-06-30 04:31:31 +0700

    Add Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,13 @@
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: 'https://6070ba25dd0d563efd4f6d4f12dee094@o4509441212153856.ingest.de.sentry.io/4509584034955344',
+  sendDefaultPii: true,
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+});
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // 👈 важно для GitHub Pages
-  plugins: [react()],
+  // 👈 важно для GitHub Pages
+  base: '/',
+
+  plugins: [react(), sentryVitePlugin({
+    org: "pesherkino",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
